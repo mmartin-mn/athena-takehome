618aa53aefea4f7cc308c9e023c39a8877415a7f
mmartin-mn <mjmartin3394@gmail.com>
2024-07-07 13:36:28 -0500

    Adding chart and yearly income calculation

--- src/social-security-aged-card/SocialSecurityAgedCard.tsx
+++ src/social-security-aged-card/SocialSecurityAgedCard.tsx
@@ -1,115 +1,10 @@
 import React, { useEffect, useState } from 'react';
+import 'chart.js/auto';
 import Select from 'react-select';
-import styled from 'styled-components'
+import { Doughnut } from 'react-chartjs-2';
 import { Options, UserData } from './types';
-
-const Container = styled.div`
-  display: flex;
-  flex-direction: column;
-  width: 420px;
-  padding: 20px;
-  align-items: center;
-`
-
-const Header = styled.h1`
-  color: #1B1D3D;
-  font-size: 20px;
-  line-height: 32px;
-  font-family: Poppins;
-  font-weight: bold;
-  letter-spacing: 1px;
-  margin: 0;
-`
-
-const SubHeader = styled.h2`
-  color: #1B1D3D;
-  font-family: Poppins;
-  font-size: 18px;
-  line-height: 24px;
-  font-weight: normal;
-  letter-spacing: 0.5px;
-  margin-top: 10px;
-  margin-bottom: 10px;
-`
-
-const Divider = styled.hr`
-  border-top: 2px solid #E2E1E1;
-  border-radius: 2px;
-  width: 100%;
-`
-
-const HouseholdContainer = styled.div`
-  width: 300px;
-  align-items: center;
-`
-
-const IdealContainer = styled.div`
-  margin-top: 20px;
-  display: flex;
-  justify-content: space-between;
-  width: 360px;
-`
-
-const Text = styled.p`
-  margin: 0;
-  color: #1B1D3D;
-  font-family: Poppins;
-  font-size: 12px;
-  line-height: 18px;
-  font-weight: normal;
-`
-
-const PaymentText = styled.p`
-  margin: 0;
-  color: #707070;
-  font-family: Poppins;
-  font-size: 20px;
-  line-height: 32px;
-  font-weight: normal;
-  margin-top: 20px;
-`
-
-const AgeInput = styled.input`
-  color: #707070;
-  font-family: Poppins;
-  font-size: 24px;
-  line-height: 32px;
-  font-weight: bold;
-  border: none;
-  border-bottom: 1.5px solid #323456;
-  outline: none;
-  text-align: center;
-  margin-top: 20px;
-`
-
-const SecondaryButton = styled.button`
-  border: 1px solid #1B1D3D;
-  color: #1B1D3D;
-  background: none;
-  width: 175px;
-  height: 40px;
-  border-radius: 20px;
-  font-family: Poppins;
-  font-size: 16px;
-  line-height: 24px;
-  letter-spacing: 1.25px;
-  font-weight: normal;
-`
-
-const PrimaryButton = styled.button`
-  border: 1px solid #1B1D3D;
-  color: #45EEE9;
-  background: #1B1D3D;
-  width: 175px;
-  height: 40px;
-  border-radius: 20px;
-  font-family: Poppins;
-  font-size: 16px;
-  line-height: 24px;
-  letter-spacing: 1.25px;
-  font-weight: normal;
-  margin-left: 40px;
-`
+import { dollarFormatter } from './utils';
+import { AgeInput, ChartContainer, Container, Divider, Header, HouseholdContainer, IdealContainer, IncomeContainer, IncomeTitle, PaymentText, PrimaryButton, SecondaryButton, SubHeader, Text } from './styled-components';
 
 const SOCIAL_SECURITY_RETIRE_AGE = 67
 const SOCIAL_SECURITY_INCOME = 18000
@@ -119,6 +14,7 @@ export const SocialSecurityAgedCard = () => {
   const [options, setOptions] = useState<Options>()
   const [selectedUser, setSelectedUser] = useState<UserData>()
   const [idealRetireAge, setIdealRetireAge] = useState<number>()
+  const [yearlyIncome, setYearlyIncome] = useState<number>()
 
   useEffect(() => {
     const getData = async () => {
@@ -167,6 +63,33 @@ export const SocialSecurityAgedCard = () => {
     }
   }, [selectedUser])
 
+  useEffect(() => {
+    if(!!idealRetireAge && !!selectedUser) {
+      // I should be making this into some sort of re-usable function, because it's the same as above mostly, but due to time I'm not
+      const birthday = new Date(selectedUser.user_info.date_of_birth).getTime()
+      const now = new Date().getTime()
+      const diff = now - birthday
+      let age = Math.floor(diff / (1000 * 60 * 60 * 24 * 365))
+      const lifeExpentancy = selectedUser.assumptions.life_expectancy
+
+      const savingsAmountPerYear = +(selectedUser.user_info.household_income * (selectedUser.user_info.current_savings_rate / 100)).toFixed(2)
+      let savingsSum = selectedUser.user_info.current_retirement_savings
+      const rate = 1 + (selectedUser.assumptions.expected_rate_of_return / 100)
+
+      while (age <= idealRetireAge) {
+        savingsSum += savingsAmountPerYear
+        savingsSum *= rate
+        savingsSum = +savingsSum.toFixed(2)
+        age++
+      }
+
+      setYearlyIncome(+(savingsSum / (lifeExpentancy - idealRetireAge)).toFixed(2))
+    } else {
+      // Reset
+      setYearlyIncome(0)
+    }
+  }, [idealRetireAge, selectedUser])
+
   const canRetire = (age: number, targetIncome: number, totalSavings: number, lifeExpentancy: number) => {
     // This variable name is unfortunate lol, but how many years they are going to be alive in retirement
     const yearsAlive = lifeExpentancy - age
@@ -199,6 +122,23 @@ export const SocialSecurityAgedCard = () => {
     <Container>
       <Header>Best Social Security Claimed Age</Header>
       <SubHeader>Our Recommendation</SubHeader>
+      {!!selectedUser && !!yearlyIncome && 
+        <ChartContainer>
+          <IncomeContainer>
+            <IncomeTitle>Estimated Household Annual Income</IncomeTitle>
+            <Header>{dollarFormatter.format(yearlyIncome)}</Header>
+          </IncomeContainer>
+          <Doughnut data={{
+            labels: [`${selectedUser?.user_info.full_name} - ${dollarFormatter.format(yearlyIncome)}`],
+            datasets: [{
+              label: selectedUser?.user_info.full_name,
+              data: [yearlyIncome],
+            }],
+          }} options={{
+            cutout: '80%'
+          }} />
+        </ChartContainer>
+      }
       <Divider />
       <HouseholdContainer>
         <Text>Household Members</Text>
@@ -220,8 +160,8 @@ export const SocialSecurityAgedCard = () => {
             </div>
           </IdealContainer>
           <div style={{ marginTop: '20px', display: 'flex', width: '100%', justifyContent: 'center' }}>
-            <SecondaryButton>Use ideal {idealRetireAge}</SecondaryButton>
-            <PrimaryButton>Accept {selectedUser?.assumptions.retirement_age}</PrimaryButton>
+            <SecondaryButton onClick={() => console.log(`${selectedUser.user_info.full_name} - ${idealRetireAge}`)}>Use ideal {idealRetireAge}</SecondaryButton>
+            <PrimaryButton onClick={() => console.log(`${selectedUser.user_info.full_name} - ${selectedUser?.assumptions.retirement_age}`)}>Accept {selectedUser?.assumptions.retirement_age}</PrimaryButton>
           </div>
         </>
       }
---
+++ src/social-security-aged-card/styled-components.ts
@@ -0,0 +1,133 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 420px;
+  padding: 20px;
+  align-items: center;
+`;
+
+export const Header = styled.h1`
+  color: #1b1d3d;
+  font-size: 20px;
+  line-height: 32px;
+  font-family: Poppins;
+  font-weight: bold;
+  letter-spacing: 1px;
+  margin: 0;
+`;
+
+export const SubHeader = styled.h2`
+  color: #1b1d3d;
+  font-family: Poppins;
+  font-size: 18px;
+  line-height: 24px;
+  font-weight: normal;
+  letter-spacing: 0.5px;
+  margin-top: 10px;
+  margin-bottom: 10px;
+`;
+
+export const Divider = styled.hr`
+  border-top: 2px solid #e2e1e1;
+  border-radius: 2px;
+  width: 100%;
+`;
+
+export const HouseholdContainer = styled.div`
+  width: 300px;
+  align-items: center;
+`;
+
+export const IdealContainer = styled.div`
+  margin-top: 20px;
+  display: flex;
+  justify-content: space-between;
+  width: 360px;
+`;
+
+export const Text = styled.p`
+  margin: 0;
+  color: #1b1d3d;
+  font-family: Poppins;
+  font-size: 12px;
+  line-height: 18px;
+  font-weight: normal;
+`;
+
+export const PaymentText = styled.p`
+  margin: 0;
+  color: #707070;
+  font-family: Poppins;
+  font-size: 20px;
+  line-height: 32px;
+  font-weight: normal;
+  margin-top: 20px;
+`;
+
+export const AgeInput = styled.input`
+  color: #707070;
+  font-family: Poppins;
+  font-size: 24px;
+  line-height: 32px;
+  font-weight: bold;
+  border: none;
+  border-bottom: 1.5px solid #323456;
+  outline: none;
+  text-align: center;
+  margin-top: 20px;
+`;
+
+export const SecondaryButton = styled.button`
+  border: 1px solid #1b1d3d;
+  color: #1b1d3d;
+  background: none;
+  width: 175px;
+  height: 40px;
+  border-radius: 20px;
+  font-family: Poppins;
+  font-size: 16px;
+  line-height: 24px;
+  letter-spacing: 1.25px;
+  font-weight: normal;
+  cursor: pointer;
+`;
+
+export const PrimaryButton = styled.button`
+  border: 1px solid #1b1d3d;
+  color: #45eee9;
+  background: #1b1d3d;
+  width: 175px;
+  height: 40px;
+  border-radius: 20px;
+  font-family: Poppins;
+  font-size: 16px;
+  line-height: 24px;
+  letter-spacing: 1.25px;
+  font-weight: normal;
+  margin-left: 40px;
+  cursor: pointer;
+`;
+
+export const ChartContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 300px;
+`;
+export const IncomeContainer = styled.div`
+  position: absolute;
+  width: 200px;
+  text-align: center;
+`;
+
+export const IncomeTitle = styled.p`
+  margin: 0;
+  margin-top: 30px;
+  color: #7c7c7c;
+  font-family: Poppins;
+  font-size: 12px;
+  line-height: 20px;
+  font-weight: normal;
+`;
